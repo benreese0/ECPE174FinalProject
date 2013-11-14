f24d20fa92f215e368afcb9d6392dae25d832503
jyl11002 <james1002@gmail.com>
2013-11-14 13:07:57 -0800

    Added AI file

---
+++ ai.sv
@@ -0,0 +1,5 @@
+/********************************************************
+********* Computer Player aka A.I. **********************
+* This file will be used to have a computer player to 
+* compete against. It will have the ball position as an input
+* and a level 
