60fa3103a7c84c3cdab357767d73395a07a33ad8
jennvalencia <j_valencia2@u.pacific.edu>
2013-11-25 14:55:16 -0800

    just started ball

--- Ball.sv
+++ Ball.sv
@@ -1,9 +1,17 @@
 /***************************************************************
  * Ball
  * 
- * <DESCRIPTION OF FILE>
+ * The following file tracks the ball position, the integer output
+ * is the center pixel of the ball.
  * 
  * Project: ECPE 174: Advanced Digital Design Final Project
- * Author:<AUTHOR NAME>
+ * Author:Jennifer Valencia 
  * Date: 2013-11-08
  ***************************************************************/
+ module Ball(input logic clk, rst);
+ 
+ always_ff @ (posedge clk or negedge rst) begin
+ 
+ 
+ endmodule
+ 
