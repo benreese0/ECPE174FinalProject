80e141d8772ba30f995f31bd74c8f9a96d803d67
smills5 <s_mills2@u.pacific.edu>
2013-12-03 12:51:03 -0800

    Updated pongvga so it has no errors

--- PongVGA.sv
+++ PongVGA.sv
@@ -19,17 +19,17 @@ module PongVGA(input logic clk, quadA, quadB,
 	
 	//linking with the hvsync
 	hvsync syncgen(.clk(clk),.vga_h_sync(vga_h_sync),.vga_v_sync(vga_v_sync),
-						.DisplayArea(DisplayArea),.CounterX(CounterX),.CounterY(CounterY));
+						.DisplayArea(DisplayArea),.CounterXout(CounterX),.CounterYout(CounterY));
 	
-	logic [8:0] PaddlePosition; //The padde position
-	logic [2:0] quadAr, quadBr;//?
+		logic [8:0] PaddlePosition; //The padde position
+		logic [2:0] quadAr, quadBr;//?
+		
+		always_ff @(posedge clk)
+			quadAr <= {quadAr[1:0], quadA};
+		always_ff @(posedge clk)
+			quadBr <= {quadBr[1:0], quadB};
 	
-	always_ff @(posedge clk)
-		quadAr <= {quadAr[1:0], quadA};
-	always_ff @(posedge clk)
-		quadBr <= {quadBr[1:0], quadB};
-	
-	always_ff @(posedge clk)
+		always_ff @(posedge clk)
 		if(quadAr[2] ^ quadAr[1] ^ quadBr[2] ^ quadBr[1])
 		begin
 			if(quadAr[2] ^ quadBr[1])
@@ -44,71 +44,109 @@ module PongVGA(input logic clk, quadA, quadB,
 			end
 		end
 		//////////////////////////////////////////////////
-		logic border = (CounterX[9:3]==0||(CounterX[9:3]==79||(CounterY[8:3]==0)||(CounterY[8:3]==59);
-		logic paddle = (CounterX>=PaddlePosition+8) && (CounterX<=PaddlePosition+120) && (CounterY[8:4]==27);
-		logic BouncingObject = border|paddle;//active if the border or paddle is redrawing itself
+		logic [9:0] ballX;
+		logic [8:0] ballY;
+		logic ball_inX, ball_inY;
+		
+		always_ff @(posedge clk)
+		begin
+			if(ball_inX==0)
+				ball_inX<=(CounterX==ballX)& ball_inY;
+			else
+				ball_inX<= !(CounterX==ballX+16);
+		end
+		
+		always_ff @(posedge clk)
+		begin
+			if(ball_inY==0) 
+				ball_inY <= (CounterY==ballY); 
+			else
+				ball_inY <= !(CounterY==ballY+16);
+		end
+		
+		logic ball;
+		assign ball = ball_inX & ball_inY;
+		//////////////////////////////////////////////////
+		logic border; 
+		assign border = ((CounterX[9:3]==0)||(CounterX[9:3]==79)||(CounterY[8:3]==0)||(CounterY[8:3]==59));
+		logic paddle;
+		assign paddle = (CounterX>=PaddlePosition+8) && (CounterX<=PaddlePosition+120) && (CounterY[8:4]==27);
+		logic BouncingObject;
+		assign BouncingObject = border|paddle;//active if the border or paddle is redrawing itself
 		
 		logic ResetCollision;
 		always_ff @(posedge clk)
-			resetCollision<=(CounterY==500)&(CounterX==0);//active only once for every video fram
+			ResetCollision<=(CounterY==500)&(CounterX==0);//active only once for every video fram
 		
-		logic Collisionx1,Collisionx2, Collisiony1, Collsiony2;
+		logic CollisionX1,CollisionX2, CollisionY1, CollisionY2;
 		always_ff @(posedge clk)
 		begin
-			if(ResetCollision) CollisionX1<=0; 
-			else if(BouncingObject & (CounterX==ballX) & (CounterY==ballY+ 8)) CollisionX1<=1;
+			if(ResetCollision) 
+				CollisionX1<=0; 
+			else if(BouncingObject & (CounterX==ballX) & (CounterY==ballY+ 8)) 
+				CollisionX1<=1;
 		end
 		always_ff @(posedge clk) 
 		begin
-			if(ResetCollision) CollisionX2<=0; 
-			else if(BouncingObject & (CounterX==ballX+16) & (CounterY==ballY+ 8)) CollisionX2<=1;
+			if(ResetCollision) 
+				CollisionX2<=0; 
+			else if(BouncingObject & (CounterX==ballX+16) & (CounterY==ballY+ 8)) 
+				CollisionX2<=1;
 		end
 		always_ff @(posedge clk) 
 		begin
-			if(ResetCollision) CollisionY1<=0; 
-			else if(BouncingObject & (CounterX==ballX+ 8) & (CounterY==ballY   )) CollisionY1<=1;
+			if(ResetCollision) 
+				CollisionY1<=0; 
+			else if(BouncingObject & (CounterX==ballX+ 8) & (CounterY==ballY)) 
+				CollisionY1<=1;
 		end
 		always_ff @(posedge clk) 
 		begin
-			if(ResetCollision) CollisionY2<=0; 
-			else if(BouncingObject & (CounterX==ballX+ 8) & (CounterY==ballY+16)) CollisionY2<=1;
+			if(ResetCollision) 
+				CollisionY2<=0; 
+			else if(BouncingObject & (CounterX==ballX+ 8) & (CounterY==ballY+16)) 
+				CollisionY2<=1;
 		end
 		
 		//////////////////////////////////////////////////////
-		logic UpdateBallPosition=ResetCollionsion;//update the ball position at the same time of collision detectors
+		logic UpdateBallPosition;
+		assign UpdateBallPosition=ResetCollision;//update the ball position at the same time of collision detectors
 		logic ball_dirx, ball_diry;
 		always_ff @(posedge clk)
 		if(UpdateBallPosition)
 		begin
-			if(~(Collisionx1&Collisionx2))//if collision on both x sides, dont move the x direction
+			if(~(CollisionX1&CollisionX2))//if collision on both x sides, dont move the x direction
 			begin
-				ballx<=ballx+(ball_dirx ? -1:1);
-				if(Collisionx2) 
+				ballX<=ballX+(ball_dirx ? -1:1);
+				if(CollisionX2) 
 					ball_dirx<=1;
-				else if (Colllisionx1) 
+				else if (CollisionX1) 
 					ball_dirx<=0;
 			end
-			if(~(Collisoiny1&Collisiony2))//if collison on both y sides, dont move in the y direction
+			if(~(CollisionY1&CollisionY2))//if collison on both y sides, dont move in the y direction
 			begin
-				bally<=bally+(ball_diry?-1:1);
-				if(Collisiony2)
+				ballY<=ballY+(ball_diry?-1:1);
+				if(CollisionY2)
 					ball_diry<=1;
-				else if(Collisiony1)
+				else if(CollisionY1)
 					ball_diry<=0;
 			end
 		end
 		
 		//////////////////////////////////////////////////
-		logic R=BouncingObject | ball | (Counter[3] ^ CounterY[3]);
-		logic G=BouncingObject | ball;
-		logic B=BouncingObject | ball;
+		logic R;
+		assign R=BouncingObject | ball | (CounterX[3] ^ CounterY[3]);
+		logic G;
+		assign G=BouncingObject | ball;
+		logic B;
+		assign B=BouncingObject | ball;
 		
 		logic vga_r, vga_g, vga_b;
 		always_ff @(posedge clk)
 		begin
-			vga_r<=R & inDisplayArea;
-			vga_g<=G & inDisplayArea;
-			vga_b<=B & inDisplayArea;
+			vga_r<=R & DisplayArea;
+			vga_g<=G & DisplayArea;
+			vga_b<=B & DisplayArea;
 		end
 		
 endmodule
